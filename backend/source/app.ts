bd0bcf15b4afd7c4392bbf4624b4b1fabc585ee8
MathGueff <gueffmatheus@gmail.com>
2026-03-15 17:43:03 -0300

    fix: remove unused import

--- backend/source/app.ts
+++ backend/source/app.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connect from './config/database'
-import AnimalsRouter from './routers/animalsRouter'
 import animalsRouter from './routers/animalsRouter'
 
 dotenv.config()
