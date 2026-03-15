f0b1403e1b29e0479e9e0dba614110897892a565
MathGueff <gueffmatheus@gmail.com>
2026-03-15 11:30:02 -0300

    feat: add prettier, eslint and configuration for backend

---
+++ backend/source/app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import connect from './config/database'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+	res.json({ message: 'Testaí Backend API is running' })
+})
+
+app.listen(port, async () => {
+	await connect()
+	console.log(`Server is running on port ${port}`)
+})
---
+++ backend/source/config/database.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+dotenv.config()
+
+export default async function connect() {
+	const uri = process.env.MONGODB_URI || ''
+	await mongoose
+		.connect(uri)
+		.then(() => {
+			console.log('Conectado ao Banco')
+		})
+		.catch((err) => {
+			console.error('Erro de conexão:', err)
+		})
+}
