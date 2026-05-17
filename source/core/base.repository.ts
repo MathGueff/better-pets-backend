5d29195ddc638a064cfa750bc9510dfae7af51bb
MathGueff <gueffmatheus@gmail.com>
2026-05-17 11:32:14 -0300

    refactor: enhance BaseRepository update method and type definitions

--- source/core/base.repository.ts
+++ source/core/base.repository.ts
@@ -1,12 +1,12 @@
-import type { Model, QueryFilter } from 'mongoose'
+import type { Model, QueryFilter, UpdateQuery } from 'mongoose'
 import { Types } from 'mongoose'
 import { BaseEntity } from '../models/entity.model'
 import { PaginatedQuery } from '../shared/pagination'
 
 export class BaseRepository<
   TEntity extends BaseEntity,
-  TCreateDTO = Omit<TEntity, keyof BaseEntity>,
-  TUpdateDTO = Partial<TCreateDTO>
+  TCreateDTO extends Omit<TEntity, keyof BaseEntity> = Omit<TEntity, keyof BaseEntity>,
+  TUpdateDTO extends Partial<TEntity> = Partial<TEntity>
 > {
   constructor(private model: Model<TEntity>) {}
 
@@ -35,13 +35,10 @@ export class BaseRepository<
     return this.model.find().select('_id')
   }
 
-  async update(id: string, updatedEntity: Partial<TUpdateDTO>) {
-    const entity = await this.model.findById(id)
-    if (!entity) {
-      return null
-    }
-    Object.assign(entity, updatedEntity)
-    return await entity.save()
+  async update(id: string, updatedEntity: TUpdateDTO) {
+    const updateQuery: UpdateQuery<TEntity> = { $set: updatedEntity }
+    const entity = await this.model.updateOne({ _id: id }, updateQuery)
+    return entity.modifiedCount > 0 ? this.findById(id) : null
   }
 
   async delete(id: string) {
