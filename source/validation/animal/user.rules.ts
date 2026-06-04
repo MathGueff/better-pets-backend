fecfcedb7fea076a8afe31bfcf42fb4f91688428
MathGueff <gueffmatheus@gmail.com>
2026-06-04 11:54:15 -0300

    fix: correct redundant types

--- source/validation/animal/user.rules.ts
+++ source/validation/animal/user.rules.ts
@@ -3,7 +3,8 @@ import { IUserInput } from '../../models/user.model'
 import { ZodEntityRules } from '../../utils/zod-types'
 
 export const userRules = {
-  name: z.string('O nome deve ser uma string').max(50, 'Nome muito grande').min(2, 'Nome muito pequeno')
-} satisfies {
-  [K in keyof ZodEntityRules<IUserInput>]: z.ZodType
-}
+  name: z
+    .string('O nome deve ser uma string')
+    .max(50, 'Nome muito grande')
+    .min(2, 'Nome muito pequeno')
+} satisfies ZodEntityRules<IUserInput>
