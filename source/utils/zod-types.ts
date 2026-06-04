fcbfabf9f2fb29b620dc86a4fa52394c6c8aa810
MathGueff <gueffmatheus@gmail.com>
2026-06-04 12:03:07 -0300

    refactor: update ZodFilterRules type to use Record instead of [K keyof T]

--- source/utils/zod-types.ts
+++ source/utils/zod-types.ts
@@ -4,6 +4,7 @@ import { SortInput } from './sorting'
 
 export type ZodEntityRules<T> = Record<keyof T, z.ZodType>
 
-export type ZodFilterRules<T> = {
-  [K in keyof Omit<T, keyof PaginationInput | keyof SortInput>]: z.ZodType
-}
+export type ZodFilterRules<T> = Record<
+  keyof Omit<T, keyof PaginationInput | keyof SortInput>,
+  z.ZodType
+>
